f63be00a53fde2814be194a077f27c4c6e518c2f
Joshua Balloch <joshuaballoch@gmail.com>
2014-03-02 16:32:22 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Airaware::Application.config.secret_token = '6778b2c9a5ff885016bc93f1584c0c7dd389dcbd67f7c4140865a775aa8274a8e79fd51480cc8919f0fd90aeff55969797423dca78a42feb64ed240f8edcad3c'
