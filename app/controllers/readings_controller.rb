1267d3a25d92be62514e00f66039143e51969bd0
Joshua Balloch <joshuaballoch@gmail.com>
2014-03-12 00:26:31 +0800

    limit to 10 in readings controller

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -2,11 +2,7 @@ class ReadingsController < ApplicationController
 
   def index
     @location = Location.find(params[:location_id])
-    if @location.readings.ordered.recent_only.count < 10
-      @readings = @location.readings.ordered.limit(10)
-    else
-      @readings = @location.readings.ordered.recent_only
-    end
+    @readings = @location.readings.ordered.limit(10)
     # TO DO: add specs, use a decorator instead of to_json
     render :json => @readings.to_json
   end
