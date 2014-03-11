5edf6423addacc9215d6f55f0aa35e44832664c4
Joshua Balloch <joshuaballoch@gmail.com>
2014-03-11 23:12:07 +0800

    show first reading first

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -4,6 +4,6 @@ class LocationsController < ApplicationController
     id = params[:id] || 1
     @location = Location.find_by_id(id)
     @readings = @location.readings.ordered
-    @last_reading = @readings.last
+    @last_reading = @readings.first
   end
 end
