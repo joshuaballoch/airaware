eb3feceaaec1af41ec7aaf164147d45e21a16eb7
Joshua Balloch <joshuaballoch@gmail.com>
2014-03-13 18:52:35 +0800

    link to survey after signup

--- app/controllers/sign_ups_controller.rb
+++ app/controllers/sign_ups_controller.rb
@@ -4,9 +4,11 @@ class SignUpsController < ApplicationController
     signup = SignUp.new(params[:sign_up])
     if signup.save
       flash.notice = "Thanks for signing up!"
+      redirect_to "https://www.surveymonkey.com/s/W8VTBYZ"
     else
       flash.alert = signup.errors.to_a.first
+      redirect_to '/demo'
     end
-    redirect_to '/demo'
+
   end
 end
