ad3923bf0e59bdeac4715d9d689895b368203cde
Joshua Balloch <joshuaballoch@gmail.com>
2014-04-20 20:42:16 +0800

    rake db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140419044348) do
+ActiveRecord::Schema.define(:version => 20140420101014) do
 
   create_table "location_users", :force => true do |t|
     t.integer  "user_id"
@@ -27,8 +27,9 @@ ActiveRecord::Schema.define(:version => 20140419044348) do
     t.string   "name"
     t.text     "description"
     t.integer  "user_id"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",                 :null => false
+    t.datetime "updated_at",                 :null => false
+    t.integer  "privacy",     :default => 0, :null => false
   end
 
   create_table "readings", :force => true do |t|
