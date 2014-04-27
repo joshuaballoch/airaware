9e589daea6c2af08b85a3283403f8e49d33a1952
Joshua Balloch <joshuaballoch@gmail.com>
2014-04-27 19:40:51 +0800

    add a service to fetch readings for airadvice api

---
+++ app/services/get_air_advice_readings_service.rb
@@ -0,0 +1,114 @@
+## Get Air Advice Readings Service
+#
+#  Function
+#    1. To check for new air advice readings via air advice API.
+
+#  Usage
+#     1. Initialize the service
+#
+#          E.g. @service = Services::GetAirAdviceReadings.new reporting_device_id: 1
+#
+#     2. Perform the service
+#
+#          E.g. @service.perform
+#
+#     3. If successful, perform will return true. The returned readings from airadvice
+#        will be already be saved into the database
+#
+#     4. If unsuccessful, perform will return false
+#
+
+class GetAirAdviceReadingsService
+  attr_accessor :readings_received, :limit, :offset
+
+  class Error < StandardError; end
+
+  class InvalidParams < Error
+
+  end
+
+  def initialize(params)
+    raise InvalidParams unless params[:reporting_device_id]
+    @device = ReportingDevice.find(params[:reporting_device_id])
+
+    @last_reading = @device.readings.ordered.first || Reading.new({reading_time: DateTime.new(2011, 11, 11)})
+    @identifier = @device.identifier
+
+    # Initialize Offset and Limit
+    @limit = 20
+    @offset = 0
+
+    # Initialize a readings array
+    @readings_received = []
+  end
+
+  def perform
+    fetch_readings
+    prep_readings
+    if valid?
+      persist!
+      true
+    else
+      false
+    end
+  end
+
+  def errors
+    @errors.uniq || []
+  end
+
+  def valid?
+    @errors = []
+    @to_save.select {|x| !x.valid? }.each do |invalid|
+      @errors << invalid.errors.to_a
+    end
+    return @errors.count > 0 ? false : true
+  end
+
+  def base_url
+    return "http://50.56.204.225:11011/readings/#{@device.identifier}"
+  end
+
+  def url
+    "#{base_url}?offset=#{@offset}&quantity=#{@limit}"
+  end
+
+  def fetch_readings
+    fetch = Curl::Easy.new(url)
+    fetch.perform
+    raw_received = JSON.parse(fetch.body_str)
+    @readings_received.concat raw_received
+    unless raw_received.count < @limit || DateTime.from_str(@readings_received.last["Timestamp"]) <= @last_reading.reading_time
+      #then we've not fetched enough. increase the offset and fetch again
+      @offset += @limit
+      fetch_readings
+    end
+  end
+
+  def clean_readings
+    @readings_received = @readings_received.select {|x| DateTime.from_str(x["Timestamp"]) > @last_reading.reading_time}
+  end
+
+  def prep_readings
+    clean_readings
+    @to_save = []
+    @readings_received.each do |reading|
+      r = @device.readings.build :reading_time => reading["Timestamp"],
+                                 :pm2p5        => reading["Part"],
+                                 :temperature  => reading["Temp"],
+                                 :humidity     => reading["Humi"],
+                                 :co2          => reading["CO2"],
+                                 :co           => reading["CO"],
+                                 :tvoc         => reading["Gas"]
+      @to_save << r
+    end
+  end
+
+  private
+    def persist!
+      @to_save.each do |r|
+        r.save!
+      end
+    end
+
+end
