732bbbff3542b0059b0a1dbd3628071be0a10c85
Joshua Balloch <joshuaballoch@gmail.com>
2014-05-14 12:31:00 +0800

    fix demo page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,21 +6,20 @@ class PagesController < ApplicationController
     else
       @location = Location.find(7)
     end
-    @readings = @location.readings.ordered
-    # @readings = @location.readings.find_by_sql(
-    #   %{
-    #     SELECT *
-    #     FROM (
-    #         SELECT
-    #             @row := @row +1 AS rownum, reading_time, temperature, humidity, hcho, co2, tvoc, pm2p5, reporting_device_id
-    #         FROM (
-    #             SELECT @row :=0) r, readings
-    #         ) ranked
-    #     WHERE rownum % 20 = 1
-    #     ORDER BY reading_time DESC
-    #     LIMIT 60
-    #   }
-    # )
+    @readings = @location.readings.find_by_sql(
+      %{
+        SELECT *
+        FROM (
+            SELECT
+                @row := @row +1 AS rownum, reading_time, temperature, humidity, hcho, co2, tvoc, pm2p5, reporting_device_id
+            FROM (
+                SELECT @row :=0) r, readings
+            ) ranked
+        WHERE rownum % 115 = 1
+        ORDER BY reading_time DESC
+        LIMIT 72
+      }
+    )
     @last_reading = @readings.first
   end
 
