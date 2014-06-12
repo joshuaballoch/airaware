6bb2cafbe97bb08d5f853dbd2524cb6cec5b43de
Joshua Balloch <joshuaballoch@gmail.com>
2014-06-12 10:35:56 +0800

    add javascript translations

--- app/assets/javascripts/locale/zh_CN/app.js
+++ app/assets/javascripts/locale/zh_CN/app.js
@@ -5,8 +5,8 @@ var locales = locales || {}; locales['zh-CN'] = {
       "": {
         "Project-Id-Version": " app 1.0.0",
         "Report-Msgid-Bugs-To": " ",
-        "POT-Creation-Date": " 2014-03-20 23:33+0800",
-        "PO-Revision-Date": " 2014-03-20 23:41+0800",
+        "POT-Creation-Date": " 2014-06-11 17:43+0800",
+        "PO-Revision-Date": " 2014-06-12 10:34+0800",
         "Last-Translator": " FULL NAME <EMAIL@ADDRESS>",
         "Language-Team": " Chinese",
         "Language": " zh_CN",
@@ -19,61 +19,141 @@ var locales = locales || {}; locales['zh-CN'] = {
         "domain": "app",
         "plural_forms": " nplurals=1; plural=0;"
       },
-      "A breath of fresh air": [
+      "(leave blank if you don't want to change it)": [
+        null,
+        "（如果不希望更改请不要填写）"
+      ],
+      "(we need your current password to confirm your changes)": [
+        null,
+        "(如需做这些更改，我们需要您当前的密码）"
+      ],
+      "Admin": [
         null,
         ""
       ],
-      "AirAware can install multiple sensors per location.": [
+      "AirAdvice": [
+        null,
+        "AirAdvice"
+      ],
+      "AirAware Demo": [
+        null,
+        "AirAware的演示"
+      ],
+      "AirAware monitors indoor air of schools, offices, and homes.": [
+        null,
+        "学校，办公室，以及家里的AirAware空气监测仪器"
+      ],
+      "AirAware yourself.": [
         null,
         ""
       ],
-      "AirAware installs air monitors in schools and, soon, in office buildings, factories, and workplaces across Shanghai and the rest of China.": [
+      "Are you sure?": [
         null,
-        ""
+        "您确定吗？"
       ],
-      "AirAware is now launching at offices and schools across China. Sign up to learn more!": [
+      "Back": [
         null,
-        "AirAware项目正在国内的学校和办公室中启动，请输入邮箱以获取更多信息！"
+        "返回"
       ],
       "Bad for Sensitive Groups (Children, Seniors)": [
         null,
         "轻度污染"
       ],
-      "Beijing Outdoor": [
+      "Buy a monitor": [
         null,
-        "北京室外"
+        "购买观测仪器"
+      ],
+      "By Email": [
+        null,
+        "使用邮箱"
       ],
       "CO2": [
         null,
         "二氧化碳"
       ],
-      "Charts and graphs enable users to take a closer look.": [
+      "Cancel my account": [
         null,
-        "用户可通过图表查看详尽数据。"
+        "取消我的账户"
       ],
-      "Clear The Air": [
+      "Change my password": [
+        null,
+        "更改密码"
+      ],
+      "Change your password": [
+        null,
+        "更改密码"
+      ],
+      "Charts give users simple, powerful ways to see recent trends.": [
+        null,
+        "图表让使用者简单并清楚的了解近期趋势"
+      ],
+      "Check your air quality!": [
+        null,
+        "查看你周围的空气质量"
+      ],
+      "Click the link below to unlock your account:": [
+        null,
+        "点击以下链接将您的账号解锁"
+      ],
+      "Confirm my account": [
+        null,
+        "确认我的账号"
+      ],
+      "Contact Email: rkw@gigabase.org": [
+        null,
+        "联络邮箱：rkw@gigabase.org"
+      ],
+      "Contact us.": [
+        null,
+        "联络我们"
+      ],
+      "Copyright 2014 AirAware": [
         null,
         ""
       ],
-      "Concerned about Air Pollution in China?": [
+      "Create a New Location": [
         null,
-        ""
+        "创建新地点"
+      ],
+      "Currently waiting confirmation for: %{unconfirmed_email}": [
+        null,
+        "等待{unconfirmed_email} 的确认"
       ],
       "Demo": [
         null,
         "演示"
       ],
-      "Filters are not enough": [
+      "Didn't receive confirmation instructions?": [
+        null,
+        "没有收到确认步骤？"
+      ],
+      "Didn't receive unlock instructions?": [
+        null,
+        "没有收到解锁步骤？"
+      ],
+      "Don't just take our word for it.": [
+        null,
+        "别光听我们的"
+      ],
+      "Edit %{resource_name}": [
+        null,
+        "修改 %{resource_name}"
+      ],
+      "Fluke": [
         null,
         ""
       ],
-      "Get AirAware.": [
+      "Forgot your password?": [
         null,
-        "获取AirAware"
+        "忘记你的密码？"
       ],
-      "Get real time indoor and outdoor air quality readings.": [
+      "Get in touch with us": [
         null,
-        "获取室内外实时空气质量数据"
+        "联络我们"
+      ],
+      "Get started in a breeze": [
+        null,
+        "立即开始"
       ],
       "Good": [
         null,
@@ -87,9 +167,289 @@ var locales = locales || {}; locales['zh-CN'] = {
         null,
         "严重污染"
       ],
+      "Hello %{email}!": [
+        null,
+        "你好%{email}!"
+      ],
       "Humidity": [
         null,
         "湿度"
+      ],
+      "If you didn't request this, please ignore this email.": [
+        null,
+        "如果你没有在等待这个邮件，请忽略。"
+      ],
+      "Indoor Air Monitoring": [
+        null,
+        "室内空气质量观测"
+      ],
+      "Indoor air pollution can't be seen.": [
+        null,
+        "室内的空气污染是看不到的。"
+      ],
+      "Interested in AirAware?": [
+        null,
+        "对AirAware有兴趣？"
+      ],
+      "It seems like you do not have any locations monitored by AirAware.": [
+        null,
+        "你选择的地点没有AirAware的一起监测"
+      ],
+      "Leave your email, phone, wechat, or qq id.": [
+        null,
+        "留下你的邮箱，微信，或者QQ账号"
+      ],
+      "Let us contact you": [
+        null,
+        "让我们联络您"
+      ],
+      "Log Out": [
+        null,
+        "登出"
+      ],
+      "Member": [
+        null,
+        "会员"
+      ],
+      "Moderate": [
+        null,
+        "良"
+      ],
+      "Oops..": [
+        null,
+        "糟糕。。。"
+      ],
+      "Our monitors are already installed in schools and offices across China.": [
+        null,
+        "我们的监测仪器已经安装在中国各地的学校及办公室"
+      ],
+      "Outdoor Comparisons": [
+        null,
+        "与室外对比"
+      ],
+      "P.M. 2.5": [
+        null,
+        "P.M. 2.5"
+      ],
+      "Peace of mind in the cloud": [
+        null,
+        "悠享闲云"
+      ],
+      "Please contact us to resolve this issue.": [
+        null,
+        "请联络我们帮助您解决这个问题"
+      ],
+      "Private": [
+        null,
+        "保密"
+      ],
+      "Public": [
+        null,
+        "公开"
+      ],
+      "Quickly compare against outdoor air pollution.": [
+        null,
+        "迅速与室外空气污染比较"
+      ],
+      "Real time data is pushed into the AirAware cloud.": [
+        null,
+        "实时数据被上传到AirAware云端"
+      ],
+      "Reporting Devices": [
+        null,
+        "空气检测设备"
+      ],
+      "Resend confirmation instructions": [
+        null,
+        "从新发送验证步骤"
+      ],
+      "Resend unlock instructions": [
+        null,
+        "从新发送解锁步骤"
+      ],
+      "Scan the QR code below, or visit": [
+        null,
+        "扫一扫，或者查看"
+      ],
+      "See what they see": [
+        null,
+        "看见他们所看见的"
+      ],
+      "Sensitive": [
+        null,
+        "敏感"
+      ],
+      "Show Legend": [
+        null,
+        "显示图例"
+      ],
+      "Sign In": [
+        null,
+        "登录"
+      ],
+      "Sign Up": [
+        null,
+        "加入会员"
+      ],
+      "Sign in": [
+        null,
+        "登录"
+      ],
+      "Sign up": [
+        null,
+        "确定"
+      ],
+      "Simple Interface": [
+        null,
+        "简单的界面"
+      ],
+      "Someone has requested a link to change your password. You can do this through the link below.": [
+        null,
+        "（有人以要求连接更改您的密码，您可以透过此连接操作）"
+      ],
+      "TVOC": [
+        null,
+        "总挥发性有机化合物"
+      ],
+      "Team Members": [
+        null,
+        "队员"
+      ],
+      "Toggle navigation": [
+        null,
+        ""
+      ],
+      "Trending Outlook - PM 2.5": [
+        null,
+        ""
+      ],
+      "Unauthorized Access!": [
+        null,
+        ""
+      ],
+      "Unhappy?": [
+        null,
+        ""
+      ],
+      "Unhealthy": [
+        null,
+        "中度污染"
+      ],
+      "Unknown": [
+        null,
+        "未知"
+      ],
+      "Unlock my account": [
+        null,
+        "将我的账号解锁"
+      ],
+      "Update": [
+        null,
+        "更新"
+      ],
+      "User Name": [
+        null,
+        "用户名"
+      ],
+      "Users": [
+        null,
+        "用户"
+      ],
+      "Users can see their indoor air quality at a glance.": [
+        null,
+        "用户可以轻松的查看室内空气品质"
+      ],
+      "Users check their air quality on phone apps and the web.": [
+        null,
+        "用户可用手机app或者网络查看空气质量"
+      ],
+      "Very Unhealthy": [
+        null,
+        "重度污染"
+      ],
+      "View the Demo": [
+        null,
+        "观看演示"
+      ],
+      "Visualize Trends": [
+        null,
+        "透过图表了解趋势"
+      ],
+      "We install.": [
+        null,
+        "我们为您安装"
+      ],
+      "You can confirm your account email through the link below:": [
+        null,
+        "您可以用以下链接验证你的用户邮箱"
+      ],
+      "You to not have permission to access %{url}.": [
+        null,
+        ""
+      ],
+      "Your account has been locked due to an excessive number of unsuccessful sign in attempts.": [
+        null,
+        "因为多次密码输入错误，您的帐号已被锁定"
+      ],
+      "Your password won't change until you access the link above and create a new one.": [
+        null,
+        "(在您点击连接创建一个新的密码前，您的密码无法更改）"
+      ],
+      "current_password": [
+        null,
+        "当前密码"
+      ],
+      "email": [
+        null,
+        "邮箱"
+      ],
+      "email, phone, wechat, or qq": [
+        null,
+        "邮箱，电话，或者qq"
+      ],
+      "first_name": [
+        null,
+        "名字"
+      ],
+      "last_name": [
+        null,
+        "姓氏"
+      ],
+      "password": [
+        null,
+        "密码"
+      ],
+      "password_confirmation": [
+        null,
+        "密码_确认"
+      ],
+      "remember me": [
+        null,
+        "记住我"
+      ],
+      "username": [
+        null,
+        "用户民"
+      ],
+      "#~ \"AirAware is now launching at offices and schools across China. Sign up to \"#~ \"learn more!\"": [
+        null,
+        "#~ \"AirAware项目正在国内的学校和办公室中启动，请输入邮箱以获取更多信息！\""
+      ],
+      "Beijing Outdoor": [
+        null,
+        "北京室外"
+      ],
+      "Charts and graphs enable users to take a closer look.": [
+        null,
+        "用户可通过图表查看详尽数据。"
+      ],
+      "Get AirAware.": [
+        null,
+        "获取AirAware"
+      ],
+      "Get real time indoor and outdoor air quality readings.": [
+        null,
+        "获取室内外实时空气质量数据"
       ],
       "Indoor Air": [
         null,
@@ -99,45 +459,13 @@ var locales = locales || {}; locales['zh-CN'] = {
         null,
         "办公室和学校室内空气质量监测"
       ],
-      "Indoor Air Quality Monitoring": [
-        null,
-        ""
-      ],
       "Indoor Air Reporting Demo": [
         null,
         "室内空气质量演示报告"
       ],
-      "Indoor air pollution can be just as bad and often worse than outdoor air, and we spend most of our days inside.": [
-        null,
-        ""
-      ],
-      "Installing air filtration is the first step toward a healthier environment, but it's not enough.": [
-        null,
-        ""
-      ],
       "Loading..": [
         null,
         "加载中..."
-      ],
-      "Many filters are not run at the right settings to get your air clean. Some may also not alleviate all indoor pollutants, like volatile organic compounds, CO2, and formaldehyde.": [
-        null,
-        ""
-      ],
-      "Moderate": [
-        null,
-        "良"
-      ],
-      "Outdoor air can be reported on-site or from nearby stations": [
-        null,
-        ""
-      ],
-      "P.M. 2.5": [
-        null,
-        ""
-      ],
-      "Paid Feature": [
-        null,
-        ""
       ],
       "Past day": [
         null,
@@ -151,57 +479,17 @@ var locales = locales || {}; locales['zh-CN'] = {
         null,
         "周"
       ],
-      "Scan the QR code below, or visit": [
-        null,
-        ""
-      ],
-      "See AirAware in action.": [
-        null,
-        ""
-      ],
       "Shanghai Outdoor": [
         null,
         "上海室外"
-      ],
-      "Show Legend": [
-        null,
-        "显示图例"
-      ],
-      "Sign up": [
-        null,
-        "确定"
-      ],
-      "TVOC": [
-        null,
-        "总挥发性有机化合物"
       ],
       "Temp": [
         null,
         "温度"
       ],
-      "Toggle navigation": [
-        null,
-        ""
-      ],
-      "Unhealthy": [
-        null,
-        "中度污染"
-      ],
-      "Very Unhealthy": [
-        null,
-        "重度污染"
-      ],
       "View the demo": [
         null,
         "去看演示"
-      ],
-      "We enable schools to monitor their air quality, and integrate with other services to provide a healthy indoor environment for their pupils.": [
-        null,
-        ""
-      ],
-      "Worry over P.M. 2.5 readings has skyrocketed in recent years, but outdoor air quality is just half the battle.": [
-        null,
-        ""
       ],
       "email, phone, or qq": [
         null,
