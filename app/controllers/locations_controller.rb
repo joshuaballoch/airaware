7bc9759f332f7641131a27b2f7f297af1ddfced0
Joshua Balloch <joshuaballoch@gmail.com>
2014-06-18 11:35:33 +0800

    remove unused variables from Locations#show

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -8,22 +8,5 @@ class LocationsController < ApplicationController
 
   def show
     @location = Location.find(params[:id])
-    # @readings = @location.readings.ordered.limit(1440)
-    @readings = @location.readings.ordered.find_by_sql(
-      ["
-        SELECT *
-        FROM (
-            SELECT
-                @row := @row +1 AS rownum, reading_time, temperature, humidity, hcho, co2, tvoc, pm2p5, reporting_device_id
-            FROM (
-                SELECT @row :=0) r, readings
-            ) ranked
-        WHERE reporting_device_id = ? AND rownum % 20 = 1
-        ORDER BY reading_time DESC
-        LIMIT 72
-      ", @location.reporting_devices.first.id]
-    )
-
-    @last_reading = @location.readings.ordered.first
   end
 end
