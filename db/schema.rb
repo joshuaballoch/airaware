9c09e766cd5d021941ad1b9ecf3c47091848bb60
Joshua Balloch <joshuaballoch@gmail.com>
2014-08-25 23:28:09 -0400

    add index of reporting device id on readings

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140721152214) do
+ActiveRecord::Schema.define(:version => 20140826032529) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "namespace"
@@ -85,6 +85,8 @@ ActiveRecord::Schema.define(:version => 20140721152214) do
     t.datetime "updated_at",          :null => false
     t.float    "co"
   end
+
+  add_index "readings", ["reporting_device_id"], :name => "index_readings_on_reporting_device_id"
 
   create_table "reporting_devices", :force => true do |t|
     t.integer  "location_id"
