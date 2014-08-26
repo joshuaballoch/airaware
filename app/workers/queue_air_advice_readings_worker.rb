34aa99dc9f712b678c764d5d8f2b8376de7c20a3
Joshua Balloch <joshuaballoch@gmail.com>
2014-08-26 01:05:27 -0400

    fetch less often for now

--- app/workers/queue_air_advice_readings_worker.rb
+++ app/workers/queue_air_advice_readings_worker.rb
@@ -2,7 +2,7 @@ class QueueAirAdviceReadingsWorker
   include Sidekiq::Worker
   include Sidetiq::Schedulable
 
-  recurrence  { hourly.minute_of_hour(0, 15, 30, 45) }
+  recurrence  { hourly.minute_of_hour(0) }
 
   def perform
     ReportingDevice.where(:device_type => ReportingDeviceType::AIR_ADVICE).find_each do |device|
